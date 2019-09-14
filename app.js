7443aacc96198b59551ff2335df94f218c30766a
diego delrieu <diego.delrieu@gmail.com>
2019-09-14 11:03:13 +0200

    fixed api and used own credentials

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
 const axios = require('axios');
 
-const clientEmail = "user2@mail.com"
-const clientPassword = "a!StrongP#assword2"
-const clientID = "775683bc70d94beaa8044c81b2f16006"
-const clientSecret = "sMgdYUzUPpo1DxbR67qP2ZbuTmU7H9gikvWPigDnQro9fk0PsRcb4EvI0iRheAJr"
-const headers = { 'Bankin-version': 2018-06-15 }
-let token = ''
+const clientEmail = 'john.doe@email.com';
+const clientPassword = 'a!StrongP455word';
+const clientID = '5ba0203954494c0182aa67e7c4f4d124';
+const clientSecret = 'QFKYNQUkPxpIyOJMAamtOcc0vPOmwE4hfVPtodZcCob0RIPp9Sv6pGFkHKDYwzy6';
+const headers = { 'Bankin-version': '2018-06-15' };
 
-// authenticate
-function login() {
-  axios.post(`https://sync.bankin.com/v2/authenticate?email=${clientEmail}&password=${clientPassword}&client_id=${clientID}&client_secret=${clientSecret}`, headers)
-    .then(function (response) {
-      token = response.access_token
+async function authenticateAndGetBalances() {
+  let token = '';
+  // authenticate
+  axios.post(`https://sync.bankin.com/v2/authenticate?email=${clientEmail}&password=${clientPassword}&client_id=${clientID}&client_secret=${clientSecret}`, {}, { headers })
+    .then((response) => {
+      token = response.data.access_token;
+      headers.Authorization = `Bearer ${token}`;
+      // Access accounts
+      return axios.get(`https://sync.bankin.com/v2/accounts?limit=10&client_id=${clientID}&client_secret=${clientSecret}`, { headers });
     })
-    .catch(function (error) {
-      console.log(error);
-    });
-  ;
-}
-
-// get account balances
-function getBalances(token) {
-  if (token !== '') {
-    axios.get(`https://sync.bankin.com/v2/accounts?limit=10&client_id=${clientID}&client_secret=${clientSecret}`, headers, token)
-    .then(function (response){
-      const accounts = response.resources
-      let totalBalance = 0
+    .then((response) => {
+      const accounts = response.data.resources;
+      let totalBalance = 0;
       accounts.forEach((account) => {
-        balance += account.balance
-      })
-      return totalBalance
+        totalBalance += account.balance;
+      });
+      return totalBalance;
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.log(error);
     });
-  } else {
-    throw error
-  }
 }
 
-login()
-getBalances(token)
+authenticateAndGetBalances();
